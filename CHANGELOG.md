88f5f8d0e1c901ff69b0ebca7057aaf8e0347449
Wenshuai Hou <wenhoujx@gmail.com>
2022-12-23 01:16:03 -0500

    update

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Change Log
+
+## [0.1.0] - 2020-12-23
+
+- Initial release.
---
+++ extension.js
@@ -0,0 +1,53 @@
+
+const vscode = require('vscode');
+
+const styles = [
+	vscode.window.createTextEditorDecorationType(
+		{
+			color: '#fff',
+			backgroundColor: '#ffbd2a',
+			overviewRulerColor: 'rgba(255,189,42,0.8)',
+		}
+	),
+]
+function beacon() {
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		console.log('onDidChangeActiveTextEditor')
+		if (!editor) {
+			return
+		}
+		_setBeacon(editor)
+	})
+	vscode.window.onDidChangeTextEditorVisibleRanges(event => {
+		console.log('onDidChangeTextEditorVisibleRanges')
+		_setBeacon(event.textEditor)
+	})
+}
+
+function _setBeacon(editor) {
+	const lineNumber = editor.selection.start.line
+	console.log(`fire beacon ${lineNumber}`)
+	editor.setDecorations(styles[0],
+		// mark the whole line 
+		[new vscode.Range(
+			new vscode.Position(lineNumber, 0),
+			new vscode.Position(lineNumber, 100),
+		)])
+	
+	setTimeout(() => _clearBeacon(), 350);
+}
+
+function _clearBeacon() {
+	vscode.window.activeTextEditor.setDecorations(styles[0], [])
+}
+
+function activate(context) {
+	context.subscriptions.push(vscode.commands.registerCommand('beacon.where-cursor', () => beacon()));
+}
+
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
